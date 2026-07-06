0afb87ac160253760c61fa8ca1955f26886a9d5e
한성준 <gggf0817@naver.com>
2026-07-06 17:19:50 +0900

    Step 3 : 스탯 관리 메뉴

--- CH2/main.cpp
+++ CH2/main.cpp
@@ -9,6 +9,7 @@ int main()
     int mp;
     int attack;
     int defence;
+    bool isGameStart = false;
 
     std::cout << "===========================================\n";
     std::cout << "   [ Dungeon Escape Text RPG ]\n";
@@ -32,7 +33,7 @@ int main()
         std::cout << "Enter Attack and Defense: (more than 20)";
         std::cin >> attack >> defence;
 
-        if (attack >= 50 && defence >= 50) {
+        if (attack >= 20 && defence >= 20) {
             break;
         }
 
@@ -42,6 +43,58 @@ int main()
     Player player(name, hp, mp, attack, defence);
 
     player.printStats();
+    player.GainHpPotion(5);
+    player.GainMpPotion(5);
+
+    std::cout << "* You received 5 HP Potions and 5 MP Potions.\n";
+    std::cout << "============================================\n";
+    std::cout << "< Character Upgrade >\n";
+    std::cout << "1. HP UP    2. MP UP    3. Attack x2\n";
+    std::cout << "4. Defense x2  5. Show Stats  0. Start Game\n";
+    std::cout << "============================================\n";
+
+    while (!isGameStart) {
+        int choose;
+        std::cout << "Choose : ";
+        std::cin >> choose;
+        if (choose < 0 && choose > 5) {
+            std::cout << "invalid input\n";
+            continue;
+        }
+
+        Stat curStat = player.GetStat();
+
+        switch (choose)
+        {
+        case 1:
+            player.UseHpPotion();
+            break;
+
+        case 2:
+            player.UseMpPotion();
+            break;
+
+        case 3:
+            player.SetAttack(curStat.Attack * 2);
+            break;
+
+        case 4:
+            player.SetDefence(curStat.Defence * 2);
+            break;
+
+        case 5:
+            player.printStats();
+            break;
+
+        case 0:
+            std::cout << "Game Start!\n";
+            isGameStart = true;
+            break;
+
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
--- CH2/player.h
+++ CH2/player.h
@@ -13,6 +13,11 @@ class Player {
 private:
     std::string name;
     Stat stat;
+    int curHpPotion = 0;
+    int curMpPotion = 0;
+
+protected:
+    
 
 public:
     Player(const std::string& name, int hp, int mp, int attack, int defence)
@@ -29,11 +34,59 @@ public:
         std::cout << "====================================\n";
     }
 
-    void setHP(int damage) {
+    void AnyDamage(int damage) {
         stat.HP -= damage;
     }
 
-    void setMP(int consumedMp) {
+    void ConsumeMP(int consumedMp) {
         stat.MP -= consumedMp;
+    }
+
+    void SetHP(int hp) {
+        stat.HP = hp;
+    }
+
+    void SetMP(int mp) {
+        stat.MP = mp;
+    }
+
+    void SetAttack(int attack) {
+        stat.Attack = attack;
+    }
+
+    void SetDefence(int defence) {
+        stat.Defence = defence;
+    }
+
+    Stat GetStat() {
+        return stat;
+    }
+
+    void GainHpPotion(int amount) {
+        curHpPotion += amount;
+    }
+
+    void GainMpPotion(int amount) {
+        curMpPotion += amount;
+    }
+
+    void UseHpPotion() {
+        if (curHpPotion <= 0) {
+            std::cout << "You don't have Hp potion.\n";
+            return;
+        }
+        stat.HP += 20;
+        curHpPotion--;
+        std::cout << "You've got 20 Hp.(Current Hp Potion: " << curHpPotion << ")\n";
+    }
+
+    void UseMpPotion() {
+        if (curMpPotion <= 0) {
+            std::cout << "You don't have Mp potion.\n";
+            return;
+        }
+        stat.MP += 20;
+        curMpPotion--;
+        std::cout << "You've got 20 Mp.(Current Mp Potion: " << curMpPotion << ")\n";
     }
 };
